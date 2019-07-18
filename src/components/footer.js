623eb33ac1acc1aa875f33c8188cbfd0ea217531
Alex Hooper <alexanderghooper@gmail.com>
2019-07-18 19:54:49 +0100

    footer style

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,20 +1,39 @@
 import React from 'react'
 import './footer.scss'
 
+import fb from '../assets/icons/social/fb.svg'
+import ig from '../assets/icons/social/ig.svg'
+import twitter from '../assets/icons/social/twitter.svg'
+
 export default function Footer() {
     return (
         <footer className="footer">
             <div className="footer__wrapper">
                 <div className="footer__wrapper__contact">
-                    <img src="" alt="Quick Fox Labs" />
+                    <h2 className="no-underline">Quick Fox Labs</h2>
                     <p>07557644480</p>
                     <p>contact@quickfoxlabs.com</p>
+                    <ul>
+                        <li><img src={fb} alt="facebook" /></li>
+                        <li><img src={ig} alt="facebook" /></li>
+                        <li><img src={twitter} alt="facebook" /></li>
+                    </ul>
                 </div>
                 <div className="footer__wrapper__location">
-                    Location
+                    <h4>Location</h4>
                 </div>
                 <div className="footer__wrapper__links">
-                    Links
+                    <h4>Quick Links</h4>
+                    <ul>
+                        <li>Brand</li>
+                        <li>About</li>
+                        <li>Services</li>
+                        <li>Case Studies</li>
+                        <li>School</li>
+                        <li>Blog</li>
+                        <li>Contact Us</li>
+                        <li>Privacy Policy</li>
+                    </ul>
                 </div>
             </div>
         </footer>
