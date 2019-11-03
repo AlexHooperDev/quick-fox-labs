c88af3a7d6c4d7d2cad00dce7b89eef066680bf1
Alex Hooper <alexanderghooper@gmail.com>
2019-11-03 19:12:03 +0000

    privacy link via src

--- src/components/CookieBanner.js
+++ src/components/CookieBanner.js
@@ -2,6 +2,7 @@ import React, { useContext, useState, useEffect } from 'react';
 import { ModalContext } from '../components/PopupState'
 
 import './cookiebanner.scss';
+import privacyPolicy from '../assets/privacy-policy.pdf';
 
 export default function CookieBanner() {
   const state = useContext(ModalContext);
@@ -14,7 +15,7 @@ export default function CookieBanner() {
   return (
     <aside className="cookie-banner" style={{ opacity: `${visible}` }}>
       <p>
-        We use cookies to give you the best online experience; their use improves our sites' functionality. By browsing this website you consent to the use of cookies. For further information regarding the use of cookies please check our <a href="/.netlify/large-media/privacy-policy.pdf" target="_blank" rel="noopener noreferrer">privacy policy.</a>
+        We use cookies to give you the best online experience; their use improves our sites' functionality. By browsing this website you consent to the use of cookies. For further information regarding the use of cookies please check our <a href={privacyPolicy} target="_blank" rel="noopener noreferrer">privacy policy.</a>
       </p>
       <button onClick={() => state.setCookieConsent()}>Continue</button>
     </aside>
